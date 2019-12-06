a4d6f1f5332a60de553eb6d5c73fca19e223bd4c
lanf0n <lanfon72@gmail.com>
2019-12-06 14:10:43 +0800

    bump version

--- aiohubot/__init__.py
+++ aiohubot/__init__.py
@@ -3,6 +3,6 @@ from .plugins import TextMessage, CatchAllMessage
 from .plugins import EnterMessage, LeaveMessage, TopicMessage
 from .robot import Robot, Blueprint
 
-__version__ = '0.4.3'
+__version__ = '0.4.4'
 __all__ = ["Robot", "Blueprint", "Adapter", "TextMessage",
            "EnterMessage", "LeaveMessage", "TopicMessage", "CatchAllMessage"]
